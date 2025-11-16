9887614c111a0159fddd12468446f1f1a8858cfb
Alejandro Rodriguez Gonzalez <alejandro.rodriguez@nodo056081.inv.usc.es>
2025-11-16 12:47:54 +0100

    plots3d

--- first_tests.py
+++ first_tests.py
@@ -21,3 +21,44 @@ plt.xlabel('x')
 plt.ylabel('y')
 
 plt.savefig('first_plots/transverse_hits.png')
+plt.close()
+
+# longitudinal hits
+
+plt.figure()
+plt.plot(df['z'], df['x'], '.')
+plt.grid(linestyle='--')
+plt.xlabel('z')
+plt.ylabel('x')
+
+plt.savefig('first_plots/longitudinal_hits.png')
+
+# 3D plot
+
+fig = plt.figure(figsize=(14,6))
+ax1 = fig.add_subplot(1,2,1, projection='3d')
+ax2 = fig.add_subplot(1,2,2, projection='3d')
+
+# add rotation for better visualization
+ax1.view_init(elev=5, azim=45)
+ax2.view_init(elev=75, azim=0)
+
+# sample 10000 points for better visualization
+
+df_sampled = df.sample(n=10000, random_state=42)
+
+ax1.scatter(df_sampled['x'], df_sampled['y'], df_sampled['z'], s=1)
+ax1.set_xlabel('x')
+ax1.set_ylabel('y')
+ax1.set_zlabel('z')
+ax1.grid(linestyle='--')
+
+ax2.scatter(df_sampled['x'], df_sampled['y'], df_sampled['z'], s=1)
+ax2.set_xlabel('x')
+ax2.set_ylabel('y')
+ax2.set_zlabel('z')
+ax2.grid(linestyle='--')
+
+fig.tight_layout()
+plt.savefig('first_plots/3D_hits.png')
+plt.close()
